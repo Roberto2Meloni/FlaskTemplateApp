aa74b5d2d41efda534c5794f6853d6e8ebd5bc8c
Roberto Meloni <roberto.meloni@axelion.ch>
2025-01-30 19:44:18 +0100

    wieter justierungen

--- apps/ToDo/routes.py
+++ apps/ToDo/routes.py
@@ -5,16 +5,41 @@ from app.config import Config
 from app.decorators import admin_required
 from .models import ToDo
 from app import db
-
+from datetime import datetime
 
 config = Config()
+
+
+def get_information(user_id):
+    # Anzahl Aufgaben bis heute von dem spezifischem Benutzer
+    # aktuelles Datum
+    # Aufgaben bis heute
+    count_task = ToDo.query.filter_by(user=user_id).count()
+    current_date = datetime.now()  # Hier das reine datetime-Objekt
+    current_task = ToDo.query.filter_by(user=user_id).all()
+
+    return count_task, current_date, current_task
 
 
 @blueprint.route("/ToDo_index", methods=["GET"])
 @admin_required
 def ToDo_index():
     app.logger.info("ToDo page accessed")
-    my_todos = ToDo.query.filter_by(user=current_user.id).all()
+
+    # Hole die Informationen vom Benutzer
+    count_task, current_date, current_task = get_information(current_user.id)
+
+    print("---LOG---")
+    print(f"Anzahl Aufgaben: {count_task}")
+    print(f"Aktuelles Datum: {current_date}")
+    print(f"Aktuelle Aufgaben: {current_task}")
+    print("---LOG---")
+
     return render_template(
-        "todo.html", user=current_user, config=config, my_todos=my_todos
+        "todo.html",
+        user=current_user,
+        config=config,
+        count_task=count_task,
+        current_date=current_date,
+        current_task=current_task,
     )
